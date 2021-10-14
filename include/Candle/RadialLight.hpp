77dd2a59c4b10f5bd3ed798250206ab08b450f8e
GuillaumeG <guillaume.guillet@outlook.com>
2021-10-14 22:06:42 +0200

    Add a rectangle detection for "out of range" line detection (#21)

--- include/Candle/RadialLight.hpp
+++ include/Candle/RadialLight.hpp
@@ -13,15 +13,15 @@ namespace candle{
     /**
      * @brief LightSource that emits light from a single point
      * @details
-     * 
+     *
      * A RadialLight is defined, mainly, by the position, the orientation, the
 	 * range of the light and the beam angle. To manipulate the
      * position and the orientation of the light, you can change the position
 	 * and rotation of the object as you would do with any sf::Transformable.
 	 * The range can be manipulated as with other LightSources, with
-     * @ref LightSource::setRange, and the angle of the beam with 
+     * @ref LightSource::setRange, and the angle of the beam with
 	 * @ref setBeamAngle.
-     * 
+     *
      * <table width="100%">
      * <tr>
      *  <td align="center"> <img src="radial_1.png" width="300px"> <br> <em>Variables schema</em> </td>
@@ -33,23 +33,23 @@ namespace candle{
     private:
         static int s_instanceCount;
         float m_beamAngle;
-        
+
         void draw(sf::RenderTarget& t, sf::RenderStates st) const override;
         void resetColor() override;
-        
+
     public:
         /**
          * @brief Constructor
          */
         RadialLight();
-        
+
         /**
          * @brief Destructor
          */
         virtual ~RadialLight();
-        
+
         void castLight(const EdgeVector::iterator& begin, const EdgeVector::iterator& end) override;
-        
+
         /**
          * @brief Set the range for which rays may be casted.
          * @details The angle shall be specified in degrees. The angle in which the rays will be casted will be
@@ -58,14 +58,26 @@ namespace candle{
          * @see getBeamAngle
          */
         void setBeamAngle(float angle);
-        
+
         /**
          * @brief Get the range for which rays may be casted.
-         * @details It defaults to 360º. 
+         * @details It defaults to 360º.
          * @see setBeamAngle
          */
         float getBeamAngle() const;
-        
+
+        /**
+         * @brief Get the local bounding rectangle of the light.
+         * @returns The local bounding rectangle in float.
+         */
+        sf::FloatRect getLocalBounds() const;
+
+        /**
+         * @brief Get the global bounding rectangle of the light.
+         * @returns The global bounding rectangle in float.
+         */
+        sf::FloatRect getGlobalBounds() const;
+
     };
 }
 
--- src/RadialLight.cpp
+++ src/RadialLight.cpp
@@ -17,18 +17,18 @@ namespace candle{
     bool l_texturesReady(false);
     std::unique_ptr<sf::RenderTexture> l_lightTextureFade;
     std::unique_ptr<sf::RenderTexture> l_lightTexturePlain;
-      
+
     void initializeTextures(){
         #ifdef CANDLE_DEBUG
         std::cout << "RadialLight: InitializeTextures" << std::endl;
         #endif
         int points = 100;
-        
+
         l_lightTextureFade.reset(new sf::RenderTexture);
         l_lightTexturePlain.reset(new sf::RenderTexture);
         l_lightTextureFade->create(BASE_RADIUS*2 + 2, BASE_RADIUS*2 + 2);
         l_lightTexturePlain->create(BASE_RADIUS*2 + 2, BASE_RADIUS*2 + 2);
-        
+
         sf::VertexArray lightShape(sf::TriangleFan, points+2);
         float step = sfu::PI*2.f/points;
         lightShape[0].position = {BASE_RADIUS + 1, BASE_RADIUS + 1};
@@ -43,20 +43,20 @@ namespace candle{
         l_lightTextureFade->draw(lightShape);
         l_lightTextureFade->display();
         l_lightTextureFade->setSmooth(true);
-        
+
         sfu::setColor(lightShape, sf::Color::White);
         l_lightTexturePlain->clear(sf::Color::Transparent);
         l_lightTexturePlain->draw(lightShape);
         l_lightTexturePlain->display();
         l_lightTexturePlain->setSmooth(true);
     }
-    
+
     float module360(float x){
         x = (float)fmod(x,360.f);
         if(x < 0.f) x += 360.f;
         return x;
     }
-    
+
     RadialLight::RadialLight()
         : LightSource()
         {
@@ -67,17 +67,17 @@ namespace candle{
         }
         m_polygon.setPrimitiveType(sf::TriangleFan);
         m_polygon.resize(6);
-        m_polygon[0].position = 
+        m_polygon[0].position =
         m_polygon[0].texCoords = {BASE_RADIUS+1, BASE_RADIUS+1};
-        m_polygon[1].position = 
+        m_polygon[1].position =
         m_polygon[5].position =
-        m_polygon[1].texCoords = 
+        m_polygon[1].texCoords =
         m_polygon[5].texCoords = {0.f, 0.f};
-        m_polygon[2].position = 
+        m_polygon[2].position =
         m_polygon[2].texCoords = {BASE_RADIUS*2 + 2, 0.f};
-        m_polygon[3].position = 
+        m_polygon[3].position =
         m_polygon[3].texCoords = {BASE_RADIUS*2 + 2, BASE_RADIUS*2 + 2};
-        m_polygon[4].position = 
+        m_polygon[4].position =
         m_polygon[4].texCoords = {0.f, BASE_RADIUS*2 + 2};
         Transformable::setOrigin(BASE_RADIUS, BASE_RADIUS);
         setRange(1.0f);
@@ -85,7 +85,7 @@ namespace candle{
         // castLight();
         s_instanceCount++;
     }
-    
+
     RadialLight::~RadialLight(){
         s_instanceCount--;
         #ifdef RADIAL_LIGHT_FIX
@@ -102,7 +102,7 @@ namespace candle{
         }
         #endif
     }
-    
+
     void RadialLight::draw(sf::RenderTarget& t, sf::RenderStates s) const{
         sf::Transform trm = Transformable::getTransform();
         trm.scale(m_range/BASE_RADIUS, m_range/BASE_RADIUS, BASE_RADIUS, BASE_RADIUS);
@@ -121,49 +121,59 @@ namespace candle{
     void RadialLight::resetColor(){
         sfu::setColor(m_polygon, m_color);
     }
-    
+
     void RadialLight::setBeamAngle(float r){
         m_beamAngle = module360(r);
     }
-    
+
     float RadialLight::getBeamAngle() const{
         return m_beamAngle;
     }
-    
+
+    sf::FloatRect RadialLight::getLocalBounds() const{
+        return sf::FloatRect(0.0f, 0.0f, m_range*4, m_range*4);
+    }
+
+    sf::FloatRect RadialLight::getGlobalBounds() const{
+        float scaledRange = m_range / BASE_RADIUS;
+        sf::Transform trm = Transformable::getTransform();
+        trm.scale(scaledRange, scaledRange, BASE_RADIUS, BASE_RADIUS);
+        return trm.transformRect( getLocalBounds() );
+    }
+
     void RadialLight::castLight(const EdgeVector::iterator& begin, const EdgeVector::iterator& end){
         float scaledRange = m_range / BASE_RADIUS;
         sf::Transform trm = Transformable::getTransform();
         trm.scale(scaledRange, scaledRange, BASE_RADIUS, BASE_RADIUS);
         std::vector<sfu::Line> rays;
-        
+
         rays.reserve(2 + std::distance(begin, end) * 2 * 3); // 2: beam angle, 4: corners, 2: pnts/sgmnt, 3 rays/pnt
-        
+
         // Start casting
         float bl1 = module360(getRotation() - m_beamAngle/2);
         float bl2 = module360(getRotation() + m_beamAngle/2);
         bool beamAngleBigEnough = m_beamAngle < 0.1f;
         auto castPoint = Transformable::getPosition();
         float off = .001f;
-        
+
         auto angleInBeam = [&](float a)-> bool {
-            return beamAngleBigEnough 
+            return beamAngleBigEnough
                    ||(bl1 < bl2 && a > bl1 && a < bl2)
                    ||(bl1 > bl2 && (a > bl1 || a < bl2));
         };
-        
+
         for(float a = 45.f; a < 360.f; a += 90.f){
             if(beamAngleBigEnough || angleInBeam(a)){
                 rays.emplace_back(castPoint, a);
             }
         }
-        
+
+        sf::FloatRect lightBounds = getGlobalBounds();
         for(auto it = begin; it != end; it++){
             auto& s = *it;
-            float d1 = s.distance(castPoint);
-            // float d2 = sfu::magnitude(s.m_origin - castPoint);
-            // float d3 = sfu::magnitude(s.point(1.f) - castPoint);
-            // if(std::max(std::min(d2, d3), d1) <= m_range*std::sqrt(2)){
-            if(d1 <= m_range){
+
+            //Only cast a ray if the line is in range
+            if( lightBounds.intersects( s.getGlobalBounds() ) ){
                 sfu::Line r1(castPoint, s.m_origin);
                 sfu::Line r2(castPoint, s.point(1.f));
                 float a1 = sfu::angle(r1.m_direction);
@@ -180,7 +190,7 @@ namespace candle{
                 }
             }
         }
-        
+
         if(bl1 > bl2){
             std::sort(
                 rays.begin(),
@@ -198,7 +208,7 @@ namespace candle{
                 rays.begin(),
                 rays.end(),
                 [bl1] (sfu::Line& r1, sfu::Line& r2){
-                    return 
+                    return
                         sfu::angle(r1.m_direction) < sfu::angle(r2.m_direction);
                 }
             );
@@ -207,7 +217,7 @@ namespace candle{
             rays.emplace(rays.begin(), castPoint, bl1);
             rays.emplace_back(castPoint, bl2);
         }
-        
+
         sf::Transform tr_i = trm.getInverse();
         // keep only the ones within the area
         std::vector<sf::Vector2f> points;
@@ -225,7 +235,7 @@ namespace candle{
         sf::Vector2f al2(std::cos(bl2rad), std::sin(bl2rad));
         int d_n = points.size()*2 + 4;
         m_debug.resize(d_n);
-        m_debug[d_n-1].color = m_debug[d_n-2].color = sf::Color::Cyan; 
+        m_debug[d_n-1].color = m_debug[d_n-2].color = sf::Color::Cyan;
         m_debug[d_n-3].color = m_debug[d_n-4].color = sf::Color::Yellow;
         m_debug[d_n-1].position = m_debug[d_n-3].position = m_polygon[0].position;
         m_debug[d_n-2].position = tr_i.transformPoint(castPoint + m_range * al1);
@@ -240,7 +250,7 @@ namespace candle{
             m_debug[i*2].position = m_polygon[0].position;
             m_debug[i*2+1].position = p;
             m_debug[i*2].color = m_debug[i*2+1].color = sf::Color::Magenta;
-#endif            
+#endif
         }
         if(beamAngleBigEnough){
             m_polygon[points.size()+1] = m_polygon[1];
