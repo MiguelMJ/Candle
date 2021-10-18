b6f76d5d7684aa418fa9f89df1c70236f0e9c599
GuillaumeG <guillaume.guillet@outlook.com>
2021-10-18 09:39:28 +0200

    Add headers file into the library target (#24)
    * docs: add Dead-Deus as a contributor for infra, bug (#16)
    
    * docs: update README.md [skip ci]
    
    * docs: update .all-contributorsrc [skip ci]
    
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    
    * Fix CMakeLists.txt (#18)
    
    Solve #17
    
    * docs: add JonathSpirit as a contributor for bug, test (#20)
    
    * docs: update README.md [skip ci]
    
    * docs: update .all-contributorsrc [skip ci]
    
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    
    * Add a git ignore file for the build directory (#22)
    
    * Add a rectangle detection for "out of range" line detection (#21)
    
    * docs: add JonathSpirit as a contributor for code, ideas (#23)
    
    * docs: update README.md [skip ci]
    
    * docs: update .all-contributorsrc [skip ci]
    
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    
    * Add candle headers in lib target
    
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    Co-authored-by: MiguelMJ <miguelmejiajimenez@hotmail.com>
    Co-authored-by: Dead-Deus <89898198+Dead-Deus@users.noreply.github.com>

--- include/Candle/geometry/Line.hpp
+++ include/Candle/geometry/Line.hpp
@@ -10,6 +10,7 @@
 #include <limits>
 
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/Rect.hpp>
 
 #include "Candle/geometry/Vector2.hpp"
 #include "Candle/Constants.hpp"
@@ -29,7 +30,7 @@ namespace sfu{
         };
         sf::Vector2f m_origin; ///< Origin point of the line.
         sf::Vector2f m_direction; ///< Direction vector (not necessarily  normalized)
-        
+
         /**
          * @brief Construct a line that passes through @p p1 and @p p2
          * @details The direction is interpreted as p2 - p1.
@@ -37,7 +38,7 @@ namespace sfu{
          * @param p2 Second point
          */
         Line(const sf::Vector2f& p1, const sf::Vector2f& p2);
-        
+
         /**
          * @brief Construct a line defined by a point and an angle.
          * @details The direction is interpreted as {cos(angle), sin(angle)}.
@@ -45,7 +46,16 @@ namespace sfu{
          * @param angle Angle defining the line
          */
         Line(const sf::Vector2f& p, float angle);
-        
+
+        /**
+         * @brief Get the global bounding rectangle of the line.
+         * @details The returned rectangle is in global coordinates, which
+         * means that it takes into account the transformations (translation,
+         * rotation, scale, ...) (see SFML).
+         * @returns Global bounding rectangle in float
+         */
+        sf::FloatRect getGlobalBounds() const;
+
         /**
          * @brief Return the relative position of a point to the line.
          * @details If the point is to the right of the direction vector, the
@@ -55,21 +65,21 @@ namespace sfu{
          * @returns -1, 0 or 1
          */
         int relativePosition(const sf::Vector2f& point) const;
-        
+
         /**
          * @brief Return the minimum distance of a point to the line.
          * @param point
          * @returns The minimum distance of a point to the line.
          */
         float distance(const sf::Vector2f& point) const;
-        
+
         /**
          * @brief Returns the relative position of one line to another.
          * @param line
          * @returns The relative position of the two lines.
          */
         LineRelativePosition intersection(const Line& line) const;
-        
+
         /**
          * @brief Returns the relative position of one line to another.
          * @details If the relative position is SECANT, the output argument
@@ -81,12 +91,12 @@ namespace sfu{
          * @see point
          */
         LineRelativePosition intersection(const Line& line, float& t) const;
-        
+
         /**
          * @brief Returns the relative position of one line to another.
          * @details If the relative position is SECANT, the output argument
          * @p t1 contains the parameter required to get the intersection
-         * point with **this** line and @p t2, the parameter required to 
+         * point with **this** line and @p t2, the parameter required to
          * get it with @p line.
          * @param line
          * @param t1 (Output argument)
@@ -95,7 +105,7 @@ namespace sfu{
          * @see point
          */
         LineRelativePosition intersection(const Line& line, float& t1, float& t2) const;
-        
+
         /**
          * @brief Get a point of the line.
          * @details The point is obtained is m_origin + param * m_direction
@@ -103,20 +113,20 @@ namespace sfu{
          * @returns A point of the line
          */
         sf::Vector2f point(float param) const;
-        
+
     };
-    
+
     /**
      * @brief Cast a ray against a set of segments.
-     * @details Use a line as a ray, casted from its 
-     * [origin](@ref sfu::Line::m_origin) in its 
+     * @details Use a line as a ray, casted from its
+     * [origin](@ref sfu::Line::m_origin) in its
      * [direction](@ref sfu::Line::m_direction). It is intersected with * a set
      * of segments, represented as [Lines](sfu::Line) too, and the one closest
      * to the cast point is returned.
-     * 
+     *
      * Segments are interpreted to be delimited by the @p ray.m_origin and
      * @p ray.point(1).
-     * 
+     *
      * @param begin Iterator to the first ray.
      * @param end Iterator to the last ray.
      * @param ray
@@ -126,7 +136,7 @@ namespace sfu{
     template <typename Iterator>
     sf::Vector2f castRay(const Iterator& begin,
                          const Iterator& end,
-                         Line ray, 
+                         Line ray,
                          float maxRange=std::numeric_limits<float>::infinity()){
         float minRange = maxRange;
         ray.m_direction = sfu::normalize(ray.m_direction);
